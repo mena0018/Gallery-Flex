efd3e231fe7741f37431a1c1142480397e54a7f7
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-09 21:10:00 +0100

    Set up some animations

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Bamboos from "../img/bamboos.jpg";
 import Castle from "../img/castle.jpg";
 import Fuji from "../img/fuji.jpg";
@@ -5,15 +6,26 @@ import Lights from "../img/lights.jpg";
 import Porte from "../img/porte.jpg";
 import Store from "../img/store.jpg";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const ImageList = () => {
   const listImages = [Bamboos, Castle, Fuji, Lights, Porte, Store];
   const list = [...listImages, ...listImages, ...listImages, ...listImages];
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
 
   return (
     <ul className="max-w-6xl mx-auto flex flex-wrap gap-6 p-8">
       {list.map((image: string, index: number) => (
         <li key={index} className="h-[250px] grow">
-          <img src={image} className="w-full h-full object-cover" />
+          <img
+            src={image}
+            className="w-full h-full object-cover"
+            data-aos={index < 15 ? "zoom-out-left" : "zoom-out-right"}
+          />
         </li>
       ))}
     </ul>
