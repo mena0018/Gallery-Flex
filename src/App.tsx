5392528ccc8549255fde243e9ba7a9363e2bfce1
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-09 20:56:30 +0100

    Import of the ImageList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import ImageList from "./components/ImageList";
+
 function App() {
-  return (
-    <div className="h-[100vh] bg-slate-900">
-      <h1 className="text-center text-4xl text-gray-100 p-40">Hello</h1>
-    </div>
-  );
+  return <ImageList />;
 }
 
 export default App;
